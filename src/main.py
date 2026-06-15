1d21c4371a393a7b017c295b3457dd1b8b864a41
Rifki-NQ <rifqinf950@gmail.com>
2026-06-15 22:16:00 +0700

    feat(main): add argparse to pass adm4_code through the terminal and change how logs named and stored
    logs filename is now named after when the app was run, and save the logs file inside the logs/ folder

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+import os
 import sys
 import asyncio
 import logging
+import argparse
+from pathlib import Path
+from datetime import datetime
 from httpx import AsyncClient
 from src.core.extract import ExtractForecast
 from src.core.transform import TransformForecast
@@ -11,30 +15,53 @@ logger = logging.getLogger(__name__)
 
 
 def setup_logging() -> None:
+    # create folder for logs if not exists
+    LOGS_FOLDER = Path("logs")
+    os.makedirs(LOGS_FOLDER, exist_ok=True)
+
+    # define log filename, which is the datetime when the app run
+    LOG_FILENAME = f"{LOGS_FOLDER}/{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.log"
+
+    # define loggers level
+    LOGS_LEVEL = logging.DEBUG
+
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=LOGS_LEVEL,
         format="%(asctime)s | %(levelname)-8s | %(name)-40s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
-        handlers=[logging.FileHandler("logs.log", "w"), logging.StreamHandler()],
+        handlers=[
+            logging.FileHandler(LOG_FILENAME, "w"),
+            logging.StreamHandler(),
+        ],
     )
+
+    # supress loggers from dependencies
     logging.getLogger("httpx").setLevel(logging.WARNING)
     logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 
-async def run_app() -> None:
-    setup_logging()
+def setup_argparse() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(prog="weather")
+    parser.add_argument("--adm4", type=str, required=True)
+    return parser.parse_args()
+
+
+async def run_app(adm4_code: str) -> None:
     logger.info("App started")
     async with AsyncClient() as client:
         extractor = ExtractForecast(client)
-        transformer = TransformForecast(extractor, "32.16.20.2003")
+        transformer = TransformForecast(extractor, adm4_code)
         loader = LoadForecast(transformer)
         await loader.load_transformed_forecast()
     logger.info("App finished successfully")
 
 
+# package bootstrap
 def main() -> None:
+    setup_logging()
+    args = setup_argparse()
     try:
-        asyncio.run(run_app())
+        asyncio.run(run_app(args.adm4))
     except DomainError as e:
         logger.critical(e)
         logger.info("App finished with error")
