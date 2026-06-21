ade4057b0bf48b452fd182093b046f92228f20a6
Rifki-NQ <rifqinf950@gmail.com>
2026-06-21 16:57:13 +0700

    test(mock_extract): add MockExtractForecastBA and MockExtractAB as alternatives from MockExtractForecast

--- tests/mock_class/mock_extract.py
+++ tests/mock_class/mock_extract.py
@@ -3,6 +3,8 @@ from collections.abc import AsyncIterable
 from tests.mock_data.mock_flattened_data import (
     MOCKED_FORECAST_LOCATION_DATA_A,
     MOCKED_WEATHER_FORECAST_DATA_A,
+    MOCKED_FORECAST_LOCATION_DATA_B,
+    MOCKED_WEATHER_FORECAST_DATA_B,
 )
 from src.core.models.raw_model import RawLocation, RawForecast
 
@@ -15,5 +17,29 @@ class MockExtractForecast:
 
     async def _yield_forecast(self) -> AsyncIterable[RawForecast]:
         for data in MOCKED_WEATHER_FORECAST_DATA_A:
+            yield RawForecast(**data)
+            await asyncio.sleep(0)
+
+
+class MockExtractForecastDataBA:
+    async def get_forecast(
+        self, adm4_code: str
+    ) -> tuple[RawLocation, AsyncIterable[RawForecast]]:
+        return RawLocation(**MOCKED_FORECAST_LOCATION_DATA_B), self._yield_forecast()
+
+    async def _yield_forecast(self) -> AsyncIterable[RawForecast]:
+        for data in MOCKED_WEATHER_FORECAST_DATA_A:
+            yield RawForecast(**data)
+            await asyncio.sleep(0)
+
+
+class MockExtractForecastAB:
+    async def get_forecast(
+        self, adm4_code: str
+    ) -> tuple[RawLocation, AsyncIterable[RawForecast]]:
+        return RawLocation(**MOCKED_FORECAST_LOCATION_DATA_A), self._yield_forecast()
+
+    async def _yield_forecast(self) -> AsyncIterable[RawForecast]:
+        for data in MOCKED_WEATHER_FORECAST_DATA_B:
             yield RawForecast(**data)
             await asyncio.sleep(0)
