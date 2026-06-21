16c7de9510526a83c669f21bce37a20847ca6813
Rifki-NQ <rifqinf950@gmail.com>
2026-06-21 16:31:23 +0700

    test(mock_data): separate mock_forecast_data and mock_flattened_data

--- tests/mock_class/mock_extract.py
+++ tests/mock_class/mock_extract.py
@@ -1,8 +1,8 @@
 import asyncio
 from collections.abc import AsyncIterable
-from tests.mock_data.mock_forecast_data import (
-    MOCKED_FORECAST_LOCATION_DATA,
-    MOCKED_FLATTENED_FORECAST_DATA,
+from tests.mock_data.mock_flattened_data import (
+    MOCKED_FORECAST_LOCATION_DATA_A,
+    MOCKED_WEATHER_FORECAST_DATA_A,
 )
 from src.core.models.raw_model import RawLocation, RawForecast
 
@@ -11,9 +11,9 @@ class MockExtractForecast:
     async def get_forecast(
         self, adm4_code: str
     ) -> tuple[RawLocation, AsyncIterable[RawForecast]]:
-        return RawLocation(**MOCKED_FORECAST_LOCATION_DATA), self._yield_forecast()
+        return RawLocation(**MOCKED_FORECAST_LOCATION_DATA_A), self._yield_forecast()
 
     async def _yield_forecast(self) -> AsyncIterable[RawForecast]:
-        for data in MOCKED_FLATTENED_FORECAST_DATA:
+        for data in MOCKED_WEATHER_FORECAST_DATA_A:
             yield RawForecast(**data)
             await asyncio.sleep(0)
